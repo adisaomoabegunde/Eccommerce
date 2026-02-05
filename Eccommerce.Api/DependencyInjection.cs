822c383fe1fb6fa89ca4c541a172396f57e12d8c
adisaomoabegunde <oluwatobilobaadedapo81@gmail.com>
2026-02-05 15:05:57 +0100

    Initial Clean Architectufe solution setup

---
+++ Eccommerce.Api/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Eccommerce.Infrastructure;
+using Eccormmerce.Application;
+
+namespace Eccommerce.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                .AddInfrastructureDI();
+
+            return services;
+        }
+    }
+}
